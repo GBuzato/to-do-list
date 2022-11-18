f1e7aca0a4647f4c06ce666f879ae7e5b3b9da98
Gabriella Buzato <gabriella-buzato@hotmail.com>
2022-11-18 15:20:56 -0300

    criado input e botao criadores de tasks

---
+++ script.js
@@ -0,0 +1,18 @@
+const main = document.createElement('main');
+const body = document.getElementsByTagName('body')[0];
+body.appendChild(main);
+const sectionInputBtnAddTask = document.createElement('section');
+main.appendChild(sectionInputBtnAddTask);
+const inputTaskText = document.createElement('input');
+inputTaskText.id = 'task-text';
+sectionInputBtnAddTask.appendChild(inputTaskText);
+
+const sectionOl = document.createElement('section');
+main.appendChild(sectionOl);
+const ol = document.createElement('ol');
+sectionOl.append(ol);
+
+const btnAddTask = document.createElement('button');
+btnAddTask.id = 'create-task';
+btnAddTask.innerText = 'Criar tarefa';
+sectionInputBtnAddTask.appendChild(btnAddTask);
